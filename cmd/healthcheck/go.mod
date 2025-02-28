a3a21d7421c7b050f87405cbf76b7b3de57a92c8
Robert Tkocz <robert.tkocz-ext@leica-geosystems.com>
2025-02-28 09:12:27 +0100

    Cleaning the code from comments

--- cmd/healthcheck/go.mod
+++ cmd/healthcheck/go.mod
@@ -3,8 +3,15 @@ module healthcheck
 go 1.24.0
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/google/uuid v1.6.0
+	github.com/stretchr/testify v1.10.0
 )
 
-require filippo.io/edwards25519 v1.1.0 // indirect
+require (
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- cmd/healthcheck/main.go
+++ cmd/healthcheck/main.go
@@ -25,53 +25,6 @@ func main() {
 		slog.Error("application error", "error", err)
 		os.Exit(1)
 	}
-	// env := getEnv()
-
-	// config, err := env.parseEnv()
-	// if err != nil {
-	// 	slog.Error(
-	// 		"failed to parse environment",
-	// 		"error", err,
-	// 	)
-	// 	os.Exit(1)
-	// }
-
-	// db, err := config.Connection.ConnectDB()
-	// if err != nil {
-	// 	slog.Error(
-	// 		"failed to connect to database",
-	// 		"error", err,
-	// 	)
-	// 	os.Exit(1)
-	// }
-
-	// defer db.Close()
-
-	// config.DBInterface = db
-
-	// mux := http.NewServeMux()
-	// mux.HandleFunc("/health", config.healthHandler)
-
-	// server := &http.Server{
-	// 	Addr:         fmt.Sprintf(":%d", config.HealthPort),
-	// 	Handler:      mux,
-	// 	ReadTimeout:  httpReadTimeout,
-	// 	WriteTimeout: httpWriteTimeout,
-	// }
-
-	// slog.Info(
-	// 	"starting health check server",
-	// 	"port", config.HealthPort,
-	// )
-
-	// if err := server.ListenAndServe(); err != nil {
-	// 	slog.Error(
-	// 		"failed to start server",
-	// 		"error", err,
-	// 	)
-	// 	db.Close() // Explicitly close the DB connection before exiting
-	// 	os.Exit(1)
-	// }
 }
 
 func (c config) getLogLevel() (slog.Level, error) {
@@ -101,7 +54,6 @@ func setupServer(config config) *http.Server {
 	}
 }
 
-// func run(config *config) error {
 func run() error {
 	slog.Info(
 		"starting healthcheck",
@@ -126,8 +78,6 @@ func run() error {
 	config.DBInterface = db
 
 	server := setupServer(*config)
-
-	// not sure about that
 
 	defer server.Close()
 
